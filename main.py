eb5876153a7320919d0950dbfa75d8e42c73e40f
rocketpy <dimitr.ushakov@gmail.com>
2019-06-08 23:40:09 +0300

    updated

--- main.py
+++ main.py
@@ -165,6 +165,25 @@ with open('file_name.png', 'wb') as f:
     f.write(r.content)
 """
 
+
+# some tricks or traps
+"""
+import requests
+from bs4 import BeautifulSoup
+
+session = requests.Session()
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5)'\
+           'AppleWebKit 537.36 (KHTML, like Gecko) Chrome',
+           'Accept':'text/html,application/xhtml+xml,application/xml;'\
+           'q=0.9,image/webp,*/*;q=0.8'}
+url = 'https://www.whatismybrowser.com/'\
+'developers/what-http-headers-is-my-browser-sending'
+req = session.get(url, headers=headers)
+
+bs = BeautifulSoup(req.text, 'html.parser')
+print(bs.find('table',{'class':'table-striped'}).get_text)
+"""
+
 # lxml -  parser
 # beautifulsoup take html to python objects
 # create func for requests
