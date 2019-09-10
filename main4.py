ca833f4819f3d751cdcfe376b79bc68b1836de66
Dmytro Ushakov <dimitr.ushakov@gmail.com>
2019-09-10 19:59:37 +0300

    updated

--- main4.py
+++ main4.py
@@ -1,35 +1,31 @@
 import csv
 import time
-import random
 import requests
+from random import choice
 from bs4 import BeautifulSoup
 
 
-proxies = [{'https': 'http:// ... '},
-           {'https': 'http:// ... '},
-           {'https': 'http:// ... '},
-           {'https': 'http:// ... '},
-           {'https': 'http:// ... '},
-          ]
+proxs = {'https': 'http://159.253.110.8:59695'}
 
 user_agents = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
               'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
               'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
               'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
               'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-              'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-              'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
               'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
               'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36']
 
+
 def get_html(url):
-#    time.sleep(2)
+    # time.sleep(2)
     headers = {'User-Agent': choice(user_agents)}
-    proxies = {'http': 'http://' + choice(proxies)} 
-#   useragent = {'User-Agent': choice(user_agents)} 
-    r = requests.get(url, proxies=proxies, headers=headers, timeout=3)
-    if r.status_code == 200:
-        return r.text
+    #proxies = proxs
+    # proxies = {'https': 'http://' + choice(proxs)}
+    # useragent = {'User-Agent': choice(user_agents)}
+    r = requests.get(url, verify=False, proxies=proxs, headers=headers, timeout=5)
+    #if r.status_code == 200:
+    return r.text
+
 
 def get_data(html):
     soup = BeautifulSoup(html, 'lxml')
@@ -54,9 +50,10 @@ list_user_agents = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHT
                     'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/4.0; GTB7.4; InfoPath.3; SV1; .NET CLR 3.1.76908; WOW64; en-US)']
   """
 
+
 def main():
-    url = 'https://2ip.ua/ru/'
-    print(get_data(get_html(url)))
+    url = 'https://2ip.ua/ua/'
+    print(get_data(get_html(url)).strip())
            
 if __name__ == '__main__':
     main()
