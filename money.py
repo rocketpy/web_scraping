264987c466c4b17e02a34d942831a4e573321061
rocketpy <dimitr.ushakov@gmail.com>
2019-10-24 17:07:40 +0300

    updated

---
+++ money.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://minfin.com.ua/currency/'
+
+r = requests.get(url)
+response = r.text
+soup = BeautifulSoup(response)
+
+#  find first
+data = soup.find('table', {'class'_: 'table-auto'})
+
+#  find all
+data = soup.findAll('table', {'class'_: 'table-auto'})
+
+tr = data.find('td', {'class'_: 'responsive-hide'}).text
+
+t_r = tr[:7]
+
