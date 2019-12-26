afe2b0b4a504f6c434cd7aaf258539426f9e09cf
rocketpy <dimitr.ushakov@gmail.com>
2019-12-26 23:27:44 +0200

    modified

--- google_search.py
+++ google_search.py
@@ -1,1 +1,25 @@
+import sys
+import requests
+import pyperclip
+import webbrowser
+from bs4 import BeautifulSoup
 
+
+# searching by keyword
+def main():
+    if len(sys.argv) > 1:
+        keyword = ' '.join(sys.argv[1:])
+    else:
+        keyword = pyperclip.paste()
+
+    resp = requests.get('http://google.com/search?q=' + keyword)
+    resp.raise_for_status()
+    soup = bs4.BeautifulSoup(resp.text)
+    link_elems = soup.select('.r a')
+    num_open = min(5, len(linkElems))
+
+    for i in range(numOpen):
+        webbrowser.open('http://google.com' + link_elems[i].get('href'))
+
+if __name__ == '__main__':
+    main()
