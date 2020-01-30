052d7a1b012ad23c3fb60be54ee124c3d3e9e0d8
rocketpy <dimitr.ushakov@gmail.com>
2020-01-30 23:04:51 +0200

    modified

--- scrapy_example_1.py
+++ scrapy_example_1.py
@@ -1,1 +1,12 @@
+import scrapy
+from urllib.parse import urljoin
 
+
+class MySpider(scrapy.Spider):
+    name = "some_name"  # spider name
+    start_urls = ['http://.com']
+    
+    def parse(self, response):
+        for post_link in response.xpath('//div[@class="post mb-2"]/h2/a/@href').extract():
+            url = urljoin(response.url, post_link)
+            print(url)
