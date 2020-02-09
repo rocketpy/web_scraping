7856d42628ad437dceeee302b4dcce5a1ba5bf1d
rocketpy <dimitr.ushakov@gmail.com>
2020-02-09 22:37:36 +0200

    modified

--- scrapy_example_2.py
+++ scrapy_example_2.py
@@ -1,4 +1,21 @@
 #  in shell:  scrapy startproject name_project
 #  cd name_project
 
+#  creating a spider and URL
+#  scrapy genspider name_spider blablabla.com/posts/
 
+import scrapy
+
+
+class MySpider(scrapy.Spider):
+    name = "name of spider"
+    
+    def start_requests(self):
+        self.index = 0
+        urls = [
+            'http://blablabla.com/posts/',
+        ]
+        
+        for url in urls:
+            #  make a request to each url !!! 
+            yield scrapy.Request(url=url, callback=self.parse)
