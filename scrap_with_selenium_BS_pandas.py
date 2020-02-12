7b0c78f732f504ddf318edf1b90124e12c1f316f
rocketpy <dimitr.ushakov@gmail.com>
2020-02-12 23:44:09 +0200

    modified

---
+++ scrap_with_selenium_BS_pandas.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from selenium import webdriver
+from BeautifulSoup import BeautifulSoup
+
+
+driver = webdriver.Chrome("path to chromedriver")  # example "/user/libs/chromium-browser/chromedriver" 
+
