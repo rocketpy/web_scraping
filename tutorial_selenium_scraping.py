542f4ed63cac1647d7f8b4595f5aea3d45bf70cc
rocketpy <dimitr.ushakov@gmail.com>
2020-05-04 23:51:21 +0300

    added main code

--- tutorial_selenium_scraping.py
+++ tutorial_selenium_scraping.py
@@ -2,4 +2,10 @@ import os
 from selenium import webdriver
 
 
+path = "C:\Program Files (x86)\chromedriver.exe"
 
+driver = webdriver.Chrome(path)
+driver.get("https:// ... ")
+
+# print(driver.title)
+driver.quit()
