b26f696397b6f28909da4f6ea105f30ed819fff0
rocketpy <dimitr.ushakov@gmail.com>
2020-05-05 23:49:59 +0300

    updated

--- tutorial_selenium_scraping.py
+++ tutorial_selenium_scraping.py
@@ -4,12 +4,14 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-path = "C:\Program Files (x86)\chromedriver.exe"
+path = "C:\Program Files\chromedriver.exe"  # or any path
 
 driver = webdriver.Chrome(path)
 driver.get("https:// ... ")
 
 # print(driver.title)
 some_elem = driver.find_element_by_name("some_name")
+some_elem.send_keys("test")
+some_elem.send_keys(Keys.RETURN)
 
 driver.quit()
