25d808d54fb1d2fe76a9e1c60e7769000a696310
rocketpy <dimitr.ushakov@gmail.com>
2020-05-06 20:56:01 +0300

    updated

--- tutorial_selenium_scraping.py
+++ tutorial_selenium_scraping.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.keys import Keys
 path = "C:\Program Files\chromedriver.exe"  # or any other path
 
 driver = webdriver.Chrome(path)
+#  driver = webdriver.Firefox()
 driver.get("https:// ... ")
 # print(driver.title)  using this for test !
 
@@ -14,6 +15,17 @@ driver.get("https:// ... ")
 search = driver.find_element_by_name("some_name")  # tag name
 search.send_keys("test")
 search.send_keys(Keys.RETURN)
+
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "myDynamicElement"))
+    )
+finally:
+    driver.quit()
+
+# find elem by id
+some_elem = driver.find_element_by_id("some_name")
+print(some_elem.text)
 
 #  need a few seconds for waiting a result
 time.sleep(5)
