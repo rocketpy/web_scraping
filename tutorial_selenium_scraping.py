8b38447865f7050e2b5b1ba0a5c49dfb48d5d8c0
rocketpy <dimitr.ushakov@gmail.com>
2020-05-07 23:17:53 +0300

    updated

--- tutorial_selenium_scraping.py
+++ tutorial_selenium_scraping.py
@@ -9,20 +9,29 @@ from selenium.webdriver.support import expected_conditions as EC
 
 path = "C:\Program Files\chromedriver.exe"  # or any other path
 
-driver = webdriver.Chrome(path)
-#  driver = webdriver.Firefox()
+driver = webdriver.Chrome(path)   # driver = webdriver.Firefox()
 driver.get("https:// ... ")
 # print(driver.title)  using this for test !
 
 
-#  Clicking elements , like buttons or links !
+#  Clicking elements , like buttons or links !  for buttons need use ID ( taked from HTML )
 link = driver.find_element_by_link_text("link_name")  # name on page
 link.click()
 try:
-    some_link = WebDriverWait(driver, 10).until(  # 10 seconds
-        EC.presence_of_element_located((By.LINK_TEXT, "some_link_name")) 
-        some_link.click()
-    )
+    some_link = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "some_link_text")))
+    some_link.click()
+    some_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "ID_button")))
+    somebutton.click()
+    
+    #  for return on previuos page use :
+    driver.back()
+    
+    # for next page use :
+    driver.forward()
+    
+except:
+    driver.quit()
+# or
 finally:
     driver.quit()
 
@@ -31,7 +40,9 @@ finally:
 # search data with input on web_page ( search field )
 search = driver.find_element_by_name("some_name")  # tag name
 search.send_keys("test")
-search.send_keys(Keys.RETURN)  # 
+# for clean serch key use :
+# search.clear()
+search.send_keys(Keys.RETURN)  
 
 #  adding waits ( using for waiting a result )
 try:
