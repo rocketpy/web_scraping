b02376cf0bea4a48ecc26e48c2481279a671c6ba
rocketpy <dimitr.ushakov@gmail.com>
2020-05-19 23:53:25 +0300

    added main code

--- scraping_images.py
+++ scraping_images.py
@@ -3,5 +3,15 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 
+URL = ""
 
+response = requests.get(URL)
+soup = BeautifulSoup(response.content, "html.parser")
+imgs = soup.find_all("img", attrs={"class": "name_class"})
+numb = 0
+
+for im in imags:
+    img_src = im["src"]
+    urllib.request.urlretrieve(img_src, str(nummb))
+    numb += 1
 
