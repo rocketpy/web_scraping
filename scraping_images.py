a0f3e0e08b98757dd4eb2a3149e3276fe0466e97
rocketpy <dimitr.ushakov@gmail.com>
2020-05-19 23:59:09 +0300

    updated

--- scraping_images.py
+++ scraping_images.py
@@ -15,3 +15,18 @@ for im in imags:
     urllib.request.urlretrieve(img_src, str(nummb))
     numb += 1
 
+
+#  or
+
+#...same imports
+import random
+
+# same
+
+for link in soup.find_all("img", attrs={"class": "name_class"}):
+    img_href = link.get("href")
+    img_name = str(random.randrange(1, 1000000)) + ".jpg"
+    urllib.request.urlretrieve(img_href, img_name)
+
+    
+    
