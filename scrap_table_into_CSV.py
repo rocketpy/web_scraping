865c1ca3f2100f3a8c950dc0dd348c4a4eb5f270
rocketpy <dimitr.ushakov@gmail.com>
2020-05-25 22:45:28 +0300

    updated

--- scrap_table_into_CSV.py
+++ scrap_table_into_CSV.py
@@ -3,6 +3,16 @@ import pandas as pd
 fro bs4 import BeautifulSoup
 
 
-URL = "..."
+URL = "..."  # page must have a some table !!!
+
 response = requests.get(URL)
+soup = BeautifulSoup(response.text, "html.parser")
+table = soup.find('table', {'class': 'class_name'}).tbody  # tbody is a tag
+
+rows = table.find_all('tr')
+columns = [i.text.replace('\n', '') for i in rows[0].find_all('th')]
+
+df = pd.DataFrame(columns=columns)
+
+
 
