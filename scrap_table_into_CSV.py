46db8affaf6d8522a853bbb101b92f8798fb3408
rocketpy <dimitr.ushakov@gmail.com>
2020-05-25 23:08:26 +0300

    updated

--- scrap_table_into_CSV.py
+++ scrap_table_into_CSV.py
@@ -14,5 +14,15 @@ columns = [i.text.replace('\n', '') for i in rows[0].find_all('th')]
 
 df = pd.DataFrame(columns=columns)
 
-
+for i in range(1, len(rows)):
+    tds = rows[i].find_all('td')
+    
+    if len(tds) == 4:
+        values = [tds[0].text, tds[1].text, '', tds[2].text, tds[3].text.replace('\n', '').replace('\xx', '')]
+    else:
+        values = [td.text.replace('\n', '').replace('\xx', '') for td in tds]  # use replace for cleaning a text
+        
+    df = df.append(pd.Series(values, index=columns), ignore_index=True)
+    df.to_csv(r'path to directory', 'file_name.csv', index=False)
+    
 
