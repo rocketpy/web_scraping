598ca07a70aaa1e8e02221fbb978ea270870dbf5
rocketpy <dimitr.ushakov@gmail.com>
2020-07-19 00:35:33 +0300

    updated

--- using_splinter.py
+++ using_splinter.py
@@ -1,5 +1,6 @@
 #  pip install splinter
 #  docs - https://github.com/cobrateam/splinter
+#  quick tutorial : https://splinter.readthedocs.io/en/latest/tutorial.html
 
 from splinter import Browser
 import pandas as pd
@@ -13,11 +14,13 @@ browser.visit('https://www.google.com')
 # take searchfield xpath
 search_bar_xpath = ''
 search_bar = browser.find_by_xpath(search_bar_xpath)[0]  # from list take idex 0
+browser.fill('q', 'some_text or some_words')
+# browser.find_by_id('').first.find_by_tag('input').fill('some_text')
 
 # find_by_tag(), find_by_name(), find_by_text() 
 
-# xext step , click the search button
-search_button_xpath = '' 
+# next step , click the search button
+search_button_xpath = 'xpath' 
 search_button = browser.find_by_xpath(search_button_xpath)[0] 
 search_button.click()
 
