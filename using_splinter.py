37168e65ee3e8ea9be25a4f53fbf953b4240cb4a
rocketpy <dimitr.ushakov@gmail.com>
2020-07-19 00:23:02 +0300

    updated

--- using_splinter.py
+++ using_splinter.py
@@ -10,20 +10,29 @@ browser = Browser('chrome')
 browser.driver.set_window_size(640, 480)
 browser.visit('https://www.google.com')
 
-# take searhfield xpath
-search_bar_xpath = '//*[@id="lst-ib"]
+# take searchfield xpath
+search_bar_xpath = ''
+search_bar = browser.find_by_xpath(search_bar_xpath)[0]  # from list take idex 0
 
-# I recommend using single quotes
-search_bar_xpath = '//*[@id="lst-ib"]'
+# find_by_tag(), find_by_name(), find_by_text() 
 
-# index 0 to select from the list
-search_bar = browser.find_by_xpath(search_bar_xpath)[0]
-
-# Now let's set up code to click the search button!
-search_button_xpath = '//*[@id="tsf"]/div[2]/div[3]/center/input[1]' 
+# xext step , click the search button
+search_button_xpath = '' 
 search_button = browser.find_by_xpath(search_button_xpath)[0] 
 search_button.click()
 
-search_results_xpath = '//h3[@class="r"]/a' # simple, right?
-
+search_results_xpath = '//h3[@class="r"]/a'  # all classes r with a tags
 search_results = browser.find_by_xpath(search_results_xpath)
+
+# for navigation auth pages use : fill() and click() !!!
+
+data = []
+
+for i in search_results:
+     title = search_result.text.encode('utf8')
+     link = search_result["href"]
+     scraped_data.append((title, link))
+    
+#  need add export scrapped data to CSV file with pandas    
+
+browser.quit()
