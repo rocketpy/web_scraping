efc6e3c6f3d93435ed6db63726f7031e1870a114
rocketpy <dimitr.ushakov@gmail.com>
2020-07-18 23:57:54 +0300

    updated

--- using_splinter.py
+++ using_splinter.py
@@ -6,3 +6,16 @@ import pandas as pd
 
 
 browser = Browser('chrome')
+
+browser.driver.set_window_size(640, 480)
+browser.visit('https://www.google.com')
+
+# take searhfield xpath
+search_bar_xpath = '//*[@id="lst-ib"]
+
+# I recommend using single quotes
+search_bar_xpath = '//*[@id="lst-ib"]'
+
+# index 0 to select from the list
+search_bar = browser.find_by_xpath(search_bar_xpath)[0]
+
