7f447aa86071c35dd03ac79dfe13980f3e60a67b
rocketpy <dimitr.ushakov@gmail.com>
2020-09-16 22:28:36 +0300

    updated

--- requests_async.py
+++ requests_async.py
@@ -1,5 +1,13 @@
 from requests_html import HTMLSession
+from requests_html import AsyncHTMLSession
 
 
 session = HTMLSession()
 req = session.get('https://...')
+
+asession = AsyncHTMLSession()
+async def get_pythonorg():
+          r = await asession.get('https://...')
+          return r
+
+    
