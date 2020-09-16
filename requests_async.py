f74213165d984dbe7c86972727c5300bd27ddc82
rocketpy <dimitr.ushakov@gmail.com>
2020-09-16 22:32:14 +0300

    added a main code

--- requests_async.py
+++ requests_async.py
@@ -10,4 +10,17 @@ async def get_pythonorg():
           r = await asession.get('https://...')
           return r
 
-    
+async def get_reddit():
+    r = await asession.get('https://reddit.com/')
+    return r
+
+async def get_google():
+          r = await asession.get('https://google.com/')
+          return r
+
+results = asession.run(get_pythonorg, get_reddit, get_google)
+#  check the requests all returned a 200 (success) code
+#  [<Response [200]>, <Response [200]>, <Response [200]>]
+
+for result in results:
+    print(result.html.url)
