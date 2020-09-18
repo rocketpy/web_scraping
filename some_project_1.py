ef9a4706f29c32ad5858baf3555e9c24ed61cd1c
rocketpy <dimitr.ushakov@gmail.com>
2020-09-18 23:31:05 +0300

    added a base

--- some_project_1.py
+++ some_project_1.py
@@ -7,3 +7,9 @@ import matplotlib.pyplot as plt
 
 
 sns.set()
+
+headers = ({'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
+
+URL = "https://..."
+
+response = get(URL, headers=headers)
