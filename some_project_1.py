e1e4e60723a45f719884a2fe776787a01d99c9c9
rocketpy <dimitr.ushakov@gmail.com>
2020-09-19 22:22:54 +0300

    updated

--- some_project_1.py
+++ some_project_1.py
@@ -13,7 +13,11 @@ headers = ({'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTM
 URL = "https://..."
 
 response = get(URL, headers=headers)
-
 soup = BeautifulSoup(response.text, 'html.parser')
-
 some_containers = html_soup.find_all('div', class_="searchResultProperty")
+first = some_containers[0]
+first.find_all('span')
+
+# cleaning data
+data = first.find_all('span').text
+data_cl = data.replace('xxx', '')
