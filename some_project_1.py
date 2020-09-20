1a9898e282897798e6cb37fbe57b568146c9723f
rocketpy <dimitr.ushakov@gmail.com>
2020-09-20 22:01:27 +0300

    updated

--- some_project_1.py
+++ some_project_1.py
@@ -12,6 +12,14 @@ headers = ({'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTM
 
 URL = "https://..."
 
+titles = []
+created = []
+prices = []
+condition = []
+descriptions = []
+urls = []
+thumbnails = []
+
 response = get(URL, headers=headers)
 soup = BeautifulSoup(response.text, 'html.parser')
 some_containers = html_soup.find_all('div', class_="searchResultProperty")
