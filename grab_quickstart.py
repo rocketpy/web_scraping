554c32d80b1e396845e259afa37ac2cd9936f5f8
rocketpy <dimitr.ushakov@gmail.com>
2020-09-30 23:28:43 +0300

    updated

--- grab_quickstart.py
+++ grab_quickstart.py
@@ -1,4 +1,9 @@
 #  grab is a python framework for building web scrapers
 #  official docs:  https://grablab.org/docs/
-#  installing:  pip install -U Grab
+#  installing:  pip install -U Grab  , also need install the lxml and pycurl libraries !!!
 
+from grab import Grab
+
+
+gr = Grab()
+resp = gr.go('http://...com/')
