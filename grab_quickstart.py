d8579d6c87f3bcb1d58a23d67f56806568df7366
rocketpy <dimitr.ushakov@gmail.com>
2020-10-01 22:38:24 +0300

    updated

--- grab_quickstart.py
+++ grab_quickstart.py
@@ -7,3 +7,20 @@ from grab import Grab
 
 gr = Grab()
 resp = gr.go('http://...com/')
+
+#  make a HEAD request
+g = Grab()
+g.setup(method='head')
+resp = g.go('http://google.com/robots.txt')
+print(len(resp.body))
+print(resp.headers['Content-Length'])
+
+
+#  set some methods
+"""
+g.setup(method='put')
+g.setup(method='delete')
+g.setup(method='options')
+g.setup(method='head')
+"""
+
