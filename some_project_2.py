708e87db59321131fee36f26ae0f0ff40c12756e
rocketpy <dimitr.ushakov@gmail.com>
2020-10-23 22:28:50 +0300

    updated

--- some_project_2.py
+++ some_project_2.py
@@ -1,1 +1,14 @@
+driver = webdriver.Chrome(path)
+
+driver.get("https:// ... ")
+
+# add your ID and password for VK account
+vk_number = ""
+vk_password = ""
+
+# got to previous url use:  driver.back()
+# go to next url use:  driver.forward() 
+
+driver.get('https://')
+
 
