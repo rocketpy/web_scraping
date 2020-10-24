0c02bb881a4bf64f15f695a324db16e57413cefc
rocketpy <dimitr.ushakov@gmail.com>
2020-10-24 22:47:14 +0300

    updated

--- some_project_2.py
+++ some_project_2.py
@@ -1,5 +1,5 @@
-import time
 import random
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -11,7 +11,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 EMAIL = ""
 PASSWORD = ""
 
-driver.get('https://www.swagbucks.com/')
+driver.get('https://www.')
 
 search = driver.find_element_by_xpath('//*[@id="sbxJxRegEmail"]')
 time.sleep(1)
@@ -24,24 +24,29 @@ search.send_keys(PASSWORD)
 login_button = driver.find_element_by_xpath('//*[@id="loginBtn"]')
 login_button.click() 
 
-survey = driver.find_element_by_xpath('//*[@id="mainNavMainList"]/li[1]/a')
-survey.click() 
+answer = driver.find_element_by_xpath('//*[@id="sbMainNavSectionListItemAnswer"]/a')
+answer.click() 
 
-choose = driver.find_element_by_xpath('//*[@id="middleInner"]/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div[1]/div/span[2]')
+choose = driver.find_element_by_xpath()
 choose.click() 
 
-def cycle():
-    first = driver.find_element_by_xpath('//*[@id="middleInner"]/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div[1]/div/div/span[1]/span')
-    first.click() 
+rand_int = random.randint(1, 2)
 
-    second = driver.find_element_by_xpath('//*[@id="middleInner"]/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div[1]/div/div/span[2]/span')
-    second.click()
-
-    answer = driver.find_element_by_xpath('//*[@id="middleInner"]/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div[2]/button')
-    answer.click()
-
-while chose.is_displayed():
-
-    rand_int = random.randint(0, 2)
-
-
+def cycle(rand):
+    if rand == 1:
+        first = driver.find_element_by_xpath('//*[@id="middleInner"]/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div[1]/div/div/span[1]/span')
+        first.click() 
+        time.sleep(1)
+        answer = driver.find_element_by_xpath('//*[@id="middleInner"]/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div[2]/button')
+        answer.click()
+        time.sleep(3)
+    else:
+        second = driver.find_element_by_xpath('//*[@id="middleInner"]/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div[1]/div/div/span[2]/span')
+        second.click()
+        time.sleep(1)
+        answer = driver.find_element_by_xpath('//*[@id="middleInner"]/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div[2]/button')
+        answer.click()
+        time.sleep(3)
+    
+while choose.is_displayed():
+    cycle(rand_int)
