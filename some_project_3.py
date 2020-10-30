9fb4207aa0c536a34aae04cfb57ab00bef71b68b
rocketpy <dimitr.ushakov@gmail.com>
2020-10-30 19:09:30 +0200

    added a cycle for persons url

--- some_project_3.py
+++ some_project_3.py
@@ -2,9 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 
 
+persons_url_list = []
+
 # preparing url's for parsing  
 for i in range()0, 800, 20):
     url = f'https://www.../../page={i}'
     req = requests.get(url)
     result = req.content
+    
+    soup = BeautifulSoup(result, 'lxml')
+    persons = soup.find_all(class_='class_name')
+    
+    for person in persons:
+        person_page_url = person.get('href')
+        persons_url_list.append(person_page_url)
+    
     
