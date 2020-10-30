cc659493f2c08e8159bcfa38358fc0bb99f59c7c
rocketpy <dimitr.ushakov@gmail.com>
2020-10-30 19:03:23 +0200

    added a cycle for url's

--- some_project_3.py
+++ some_project_3.py
@@ -2,4 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 
-
+# preparing url's for parsing  
+for i in range()0, 800, 20):
+    url = f'https://www.../../page={i}'
+    req = requests.get(url)
+    result = req.content
+    
