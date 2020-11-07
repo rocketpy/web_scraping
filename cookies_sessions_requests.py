fc0ee6269ba18acc6277d6a1f5cdc9a8381ad3ba
rocketpy <dimitr.ushakov@gmail.com>
2020-11-07 22:17:37 +0200

    updated

--- cookies_sessions_requests.py
+++ cookies_sessions_requests.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 
@@ -6,12 +7,21 @@ import requests
 
 session = requests.Session()
 
+#  get cookies from Request Headers and take part like : chat_session=value
+
 #  create a cookies jar for contain a cookies
 jar = requests.cookies.RequestsCookieJar()
 
+cookie_name = "chat_session"
+cookie_value = "some value"
+
+jar.set("cookie_name", "cookie_value")
+
+session.cookies = jar
+
 r = session.get("URL")
 
+js = json.loads(r.text)
+print(js['HTML'][0])
+
 # r = requests.get("URL")
-
-#  get cookies from Request Headers and take part like : chat_session=
-
