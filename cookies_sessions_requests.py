14531aec9a1aa14c4f8a40f2e3e5c1df20a94518
rocketpy <dimitr.ushakov@gmail.com>
2020-11-07 22:06:29 +0200

    updated

--- cookies_sessions_requests.py
+++ cookies_sessions_requests.py
@@ -4,5 +4,14 @@ import requests
 #  how take cookies ??? 
 # 1. open dev. console on website , 2. go to Networks , 3. go to Headers , here we can take a cookies 
 
+session = requests.Session()
 
-r = requests.get("")
+#  create a cookies jar for contain a cookies
+jar = requests.cookies.RequestsCookieJar()
+
+r = session.get("URL")
+
+# r = requests.get("URL")
+
+#  get cookies from Request Headers and take part like : chat_session=
+
