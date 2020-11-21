d89dfbd8c0c9a7f5404fe16fed720665cef712d7
rocketpy <dimitr.ushakov@gmail.com>
2020-11-21 23:39:11 +0200

    updated

--- some_project_4.py
+++ some_project_4.py
@@ -4,5 +4,10 @@ from selenium import webdriver
 
 PATH = str(Path('geckodriver').resolve())
 
+def main():
+    driver = webdriver.Firefox(executable_path=PATH)
+    driver.get('https://')
+    
+
 if __name__ == '__main__':
     main()
