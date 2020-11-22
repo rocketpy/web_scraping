a8b8138e2d3bc15470bbde54dadaae1332c4b15a
rocketpy <dimitr.ushakov@gmail.com>
2020-11-22 18:24:51 +0200

    updated

--- some_project_4.py
+++ some_project_4.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 from selenium import webdriver
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 
 
+# PATH = "C:\Program Files\chromedriver.exe" 
+# driver = webdriver.Chrome(PATH) 
 PATH = str(Path('geckodriver').resolve())
 
-headers = {'accept':'*/*',    # accept using on web-page , where we using search 
-           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
-           
-base_url = 'https://'
-
+# headers = {'accept':'*/*',    # accept using on web-page , where we using search 
+#            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}      
+# base_url = 'https://'
 """
 def parse():
     session = requests.Session()
@@ -19,6 +19,8 @@ def parse():
 def main():
     driver = webdriver.Firefox(executable_path=PATH)
     driver.get('https://')
+    html = browser.page_source
+    soup = bs(html, 'lxml')
     
 
 if __name__ == '__main__':
