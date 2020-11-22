803c77824383ef219aa23613f6c116d6c4a30fbf
rocketpy <dimitr.ushakov@gmail.com>
2020-11-22 18:14:29 +0200

    updated

--- some_project_4.py
+++ some_project_4.py
@@ -1,8 +1,20 @@
 from pathlib import Path
 from selenium import webdriver
+from bs4 import BeautifulSoup
 
 
 PATH = str(Path('geckodriver').resolve())
+
+headers = {'accept':'*/*',    # accept using on web-page , where we using search 
+           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+           
+base_url = 'https://'
+
+"""
+def parse():
+    session = requests.Session()
+    req = session.get(base_url, headers=headers)
+"""
 
 def main():
     driver = webdriver.Firefox(executable_path=PATH)
