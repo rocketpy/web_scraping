caa982130de57346f32a05550f5133f2f6b3feed
rocketpy <dimitr.ushakov@gmail.com>
2020-12-07 22:19:42 +0200

    updated

--- robobrowser_tutorial.py
+++ robobrowser_tutorial.py
@@ -1,1 +1,11 @@
+"""
+RoboBrowser combines the best of two excellent Python libraries:
+Requests and BeautifulSoup. RoboBrowser represents browser sessions using Requests and HTML responses using BeautifulSoup,
+transparently exposing methods of both libraries
+"""
 
+# Official website: http://robobrowser.readthedocs.org/
+
+# Installation
+# mkvirtualenv robobrowser
+# pip install robobrowser
