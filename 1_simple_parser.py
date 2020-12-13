d994ffa481f8f3ad033e80bb3dba12597d387687
rocketpy <dimitr.ushakov@gmail.com>
2020-12-13 19:02:49 +0200

    updated

--- 1_simple_parser.py
+++ 1_simple_parser.py
@@ -5,3 +5,20 @@ from bs4 import BeautifulSoup
 def get_html(url):
     result = requests.get(url)
     return result.text
+
+
+def get_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+    h1 = soup.find('h1', {'class': 'class_name'})
+    a = soup.find('a', {'href': 'https://www...'})
+    navbar = soup.find('ul', {'id': 'id_name'})
+    li = navbar.find('li').find('a').text
+
+
+def main():
+    html = get_html('https://www...')
+    get_data(html)
+
+
+if __name__ == '__main__':
+    main()
