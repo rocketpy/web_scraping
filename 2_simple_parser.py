d5fb48ebba3d8d9027599b2d98e5b6b8aedf97c2
rocketpy <dimitr.ushakov@gmail.com>
2020-12-13 19:09:33 +0200

    updated

--- 2_simple_parser.py
+++ 2_simple_parser.py
@@ -5,3 +5,22 @@ from bs4 import BeautifulSoup
 def get_html(url):
     result = requests.get(url)
     return result.text
+
+
+def get_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+    section = soup.find_all('section', {'class': 'class_name'})[1]  # taking a second elem.
+    arts = section.find_all('article')  # return a list
+    for art in arts:
+        h2 = art.find('h2')
+        rate = art.find('span', {'class': 'class_name'})
+        print(h2.text, rate.text)
+
+
+def main():
+    html = get_html('https://www...')
+    data = get_data(html)
+
+
+if __name__ == '__main__':
+    main()
