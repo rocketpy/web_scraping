49054ddc86ea5140231b5e4267e1279fce033def
rocketpy <dimitr.ushakov@gmail.com>
2020-12-27 21:24:43 +0200

    Update 3_simple_parser.py

--- 3_simple_parser.py
+++ 3_simple_parser.py
@@ -4,18 +4,49 @@ from bs4 import BeautifulSoup
 
 
 def get_html(url):
-    r = requests.get(url)
-    return r.text
+    resp = requests.get(url)
+    return resp.text
 
-def refined(s):
-    r = s.split(' ')[0]
-    return r.replace(u'\xa0', '')
+
+def refined_data(data):
+    r = data.split(' ')[0]
+    return r.replace('u\xa0', '')
 
 
 def write_csv(data):
-    with open('plugins.csv', 'a') as f:
+    with open('result.csv', 'a') as f:
         writer = csv.writer(f, delimiter=',')
 
         writer.writerow((data['name'],
                          data['url'],
-                         data['reviews']))
+                         data['reviews'])
+                       )
+        
+        
+def get_data(html):
+    soup = BeautifulSoup(html , 'lxml')
+    popular = soup.find_all('section')[1]
+    plags = popular.find_all('article')
+
+    for plag in plags:
+        name = plag.find('h2').text
+        url = plag.find('h2').find('a').get('href')
+
+        r = plag.find('span', class_='rating-count').find('a').text
+        rating = refined_data(r)
+
+        data = {'name': name,
+                'url': url,
+                'reviews': rating
+               }
+
+        write_csv(data)
+
+
+def main():
+    url = 'https://www...'
+    get_data(get_html(url))
+
+
+if __name__ == '__main__':
+    main()        
