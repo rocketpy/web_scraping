e7fbd895812f32863df41d7a9eb9f709d1193011
rocketpy <dimitr.ushakov@gmail.com>
2020-12-27 21:13:03 +0200

    Update 3_simple_parser.py

--- 3_simple_parser.py
+++ 3_simple_parser.py
@@ -6,3 +6,16 @@ from bs4 import BeautifulSoup
 def get_html(url):
     r = requests.get(url)
     return r.text
+
+def refined(s):
+    r = s.split(' ')[0]
+    return r.replace(u'\xa0', '')
+
+
+def write_csv(data):
+    with open('plugins.csv', 'a') as f:
+        writer = csv.writer(f, delimiter=',')
+
+        writer.writerow((data['name'],
+                         data['url'],
+                         data['reviews']))
