a7d152aa479a01bedc4a707859a05d840a38bdeb
rocketpy <dimitr.ushakov@gmail.com>
2021-01-13 22:57:31 +0200

    Update Selenium_webdriver_mode_off.py

--- Selenium_webdriver_mode_off.py
+++ Selenium_webdriver_mode_off.py
@@ -2,6 +2,31 @@ import time
 from selenium import webdriver
 
 
-options = webdriver.ChromeOptions()
+# "C:\\users\\selenium_python\\chromedriver\\chromedriver.exe"
+# r"C:\users\selenium_python\chromedriver\chromedriver.exe"
 
+options = webdriver.ChromeOptions()
 options.add_argument("user-agent=...")
+
+#  webdriver mode OFF
+
+#  for ChromeDriver under version 79.0.3945.16
+# options.add_experimental_option("excludeSwitches", ["enable-automation"])
+# options.add_experimental_option("useAutomationExtension", False)
+
+# for ChromeDriver version 79.0.3945.16 or higher
+options.add_argument("--disable-blink-features=AutomationControlled")
+
+driver = webdriver.Chrome(executable_path="...",
+                          options=options
+                         )
+
+try:
+    driver.get("https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html")  # checking a result !
+    # time.sleep(5)
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
+ 
