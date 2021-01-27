e848f9ff45b5983a7b9ff289a55bc2670828574a
rocketpy <dimitr.ushakov@gmail.com>
2021-01-27 22:16:06 +0200

    Update and rename main.py to Base_template.py

--- Base_template.py
+++ Base_template.py
@@ -533,3 +533,4 @@ Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0; FunWebProducts)
 Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0; chromeframe/11.0.696.57)
 Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0; chromeframe/11.0.696.57)
 """
+ 
