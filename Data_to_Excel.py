cfded1ba833f3a24d2fbc3e68d497efdcfdd48cd
rocketpy <dimitr.ushakov@gmail.com>
2021-03-24 21:43:19 +0200

    Update Data_to_Excel.py

--- Data_to_Excel.py
+++ Data_to_Excel.py
@@ -1,1 +1,8 @@
+import xlsxwriter
 
+
+# pip install XlsxWriter
+
+date = time.strftime("%d%m_%H%M", time.localtime())
+writer = pd.ExcelWriter('1_'+date+'.xlsx', engine='xlsxwriter')
+print('Current time', date)
