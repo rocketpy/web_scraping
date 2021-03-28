09ecae26f54962c2b06be33a16c2cf8a9379756b
rocketpy <dimitr.ushakov@gmail.com>
2021-03-28 20:07:02 +0300

    Update Scraping_imgs.py

--- Scraping_imgs.py
+++ Scraping_imgs.py
@@ -8,13 +8,19 @@ def get_data():
               }
            
     # start from main url
-    # urls = "https://"
+    # url = "https://"
            
     #  using loop for urls
     for i in range(1, 100):
         urls = f"https://.../{i}.jpg"  # or /{i}.html
         r = requests.get(url=url, headers=headers)
         resp = r.content
+        
+        # create new dir named Images and save scraped imgs to this dir
+        with open(f"Images/{i}.jpg", "wb") as f:
+            f.write(resp)
+            # checking in cmd a progress
+            # print(f"Scraped {i} of 100 ")
 
 if __name__ == '__main__':
     get_data()
