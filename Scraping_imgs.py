20a1042f13ebaf8282826175f420d72d8c76c6ca
rocketpy <dimitr.ushakov@gmail.com>
2021-03-28 19:20:28 +0300

    Update Scraping_imgs.py

--- Scraping_imgs.py
+++ Scraping_imgs.py
@@ -1,1 +1,17 @@
+import requests
 
+
+# need take Accept and User-agent from Networkfrom Inspector (Chrome Dev tools)
+headers = {"Accept": ""
+           "User=Agent": ""
+          }
+# start from main url
+urls = "https://"
+
+def get_data():
+    
+      
+
+
+if __name__ == '__main__':
+    get_data()
