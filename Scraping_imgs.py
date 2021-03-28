4eb08659e10e769a95fa09f8d2b2dd26c817bb4c
rocketpy <dimitr.ushakov@gmail.com>
2021-03-28 20:18:57 +0300

    Update Scraping_imgs.py

--- Scraping_imgs.py
+++ Scraping_imgs.py
@@ -1,6 +1,8 @@
 import requests
+import img2pdf
 
 
+# scraping images and save to PDF file
 def get_data():
     # need take Accept and User-agent from Networkfrom Inspector (Chrome Dev tools)
     headers = {"Accept": "..."
@@ -9,9 +11,10 @@ def get_data():
            
     # start from main url
     # url = "https://"
-           
+    
+    imgs_list = []  # use this list for img2pdf module
     #  using loop for urls
-    for i in range(1, 100):
+    for i in range(1, 101):  # 100 pages
         urls = f"https://.../{i}.jpg"  # or /{i}.html
         r = requests.get(url=url, headers=headers)
         resp = r.content
@@ -19,8 +22,15 @@ def get_data():
         # create new dir named Images and save scraped imgs to this dir
         with open(f"Images/{i}.jpg", "wb") as f:
             f.write(resp)
+            imgs_list.append(f"Images/{i}.jpg")
+            
             # checking in cmd a progress
             # print(f"Scraped {i} of 100 ")
-
+            
+    # creating PDF file        
+    with open("File with images.pdf", "wb") as f:
+        f.write(img2pdf.convert(imgs_list))
+    
+    
 if __name__ == '__main__':
     get_data()
