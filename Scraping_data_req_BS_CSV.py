4fa24268bbe4a82f9e3d752889597dba6a916a57
rocketpy <dimitr.ushakov@gmail.com>
2021-03-29 21:15:55 +0300

    Update Scraping_data_req_BS_CSV.py

--- Scraping_data_req_BS_CSV.py
+++ Scraping_data_req_BS_CSV.py
@@ -1,1 +1,15 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
 
+
+
+URL = "https://"
+CSV = "file_name.csv"
+headers = {"Accept": "..."
+           "User=Agent": "..."
+          }
+
+def get_html(url, params=''):
+    req = requests.get(url=url, headers=headers, params=params)
+    return req
