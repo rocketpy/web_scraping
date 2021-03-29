bfee62f2878c8f387980a156754df07c0d2b4699
rocketpy <dimitr.ushakov@gmail.com>
2021-03-29 21:26:36 +0300

    Update Scraping_data_req_BS_CSV.py

--- Scraping_data_req_BS_CSV.py
+++ Scraping_data_req_BS_CSV.py
@@ -6,6 +6,8 @@ from bs4 import BeautifulSoup
 
 URL = "https://"
 CSV = "file_name.csv"
+
+# need take Accept and User-agent from Networkfrom Inspector (Chrome Dev tools)
 headers = {"Accept": "..."
            "User=Agent": "..."
           }
@@ -13,3 +15,17 @@ headers = {"Accept": "..."
 def get_html(url, params=''):
     req = requests.get(url=url, headers=headers, params=params)
     return req
+
+
+def get_data(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('div', class_='product_item')
+
+    elems = []
+    for item in items:
+        elems.append({'title': item.find('div', class_='title').get_text(strip=True),
+                      'product_link': URL + item.find('div', class_='title').find('a').get('href'),
+                      'img': URL + item.find('div', class_='image').find('img').get('src')})
+           
+           
+           
