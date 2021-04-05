6c6bf8640465158accdc8d9844d7d0a225df7a91
rocketpy <dimitr.ushakov@gmail.com>
2021-04-05 22:00:34 +0300

    Update Requests_Gevent.py

--- Requests_Gevent.py
+++ Requests_Gevent.py
@@ -22,4 +22,16 @@ rs = (grequests.get(u) for u in urls)
 grequests.map(rs)
 # [<Response [200]>, <Response [200]>, <Response [200]>, <Response [200]>, <Response [200]>]
 
+# Optionally, in the event of a timeout or any other exception during the connection of the request, you can add an exception handler !
+def exception_handler(request, exception):
+    print("Request failed")
+
+reqs = [grequests.get('http://httpbin.org/delay/1', timeout=0.001),
+        grequests.get('http://fakedomain/'),
+        grequests.get('http://httpbin.org/status/500')
+       ]
+grequests.map(reqs, exception_handler=exception_handler)
+# Request failed
+# Request failed
+# [None, None, <Response [500]>]
 
