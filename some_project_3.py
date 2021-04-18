8b1adef263a60445c00e5e82331c3922c7f00511
rocketpy <dimitr.ushakov@gmail.com>
2021-04-18 23:07:10 +0300

    Update some_project_3.py

--- some_project_3.py
+++ some_project_3.py
@@ -5,8 +5,8 @@ from bs4 import BeautifulSoup
 persons_url_list = []
 
 # preparing url's for parsing  
-for i in range()0, 800, 20):
-    url = f'https://www.../../page={i}'
+for i in range(0, 800, 20):
+    url = f'https://www.../page={i}'
     req = requests.get(url)
     result = req.content
     
