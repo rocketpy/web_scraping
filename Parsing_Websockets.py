1857f463e72ef5e235c267eb8dd98a67cc1d2e6c
rocketpy <dimitr.ushakov@gmail.com>
2021-04-23 23:18:43 +0300

    Update Parsing_Websockets.py

--- Parsing_Websockets.py
+++ Parsing_Websockets.py
@@ -29,5 +29,18 @@ def on_open(ws):
         for i in range(3):
             time.sleep(1)
             ws.send(data_orig)
+            
+        time.sleep(1)
+        ws.close()
+        print("thread terminating...")
+    thread.start_new_thread(run, ())
 
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("ws://*********",
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
 """
