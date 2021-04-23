76d9e2e5d669eafc33fb53bdd1dc3eeb11cfcfd8
rocketpy <dimitr.ushakov@gmail.com>
2021-04-23 23:14:56 +0300

    Update Parsing_Websockets.py

--- Parsing_Websockets.py
+++ Parsing_Websockets.py
@@ -1,1 +1,14 @@
+#  WebSocket Docs:  https://websockets.readthedocs.io/en/stable/
+
+# client sends and receives messages:
+import asyncio
+import websockets
+
+async def hello():
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as websocket:
+        await websocket.send("Hello world!")
+        await websocket.recv()
+
+asyncio.get_event_loop().run_until_complete(hello())
 
