d2b13480d546cb52da71333b3e7dc0de612ed392
rocketpy <dimitr.ushakov@gmail.com>
2021-04-29 23:15:57 +0300

    Update urllib3_tutorial.py

--- urllib3_tutorial.py
+++ urllib3_tutorial.py
@@ -1,1 +1,14 @@
+#  urllib3 is a powerful, user-friendly HTTP client
+
+#  Docs:  https://urllib3.readthedocs.io/en/latest/user-guide.html
+#  pip install urllib3
+
+import urllib3
+
+http = urllib3.PoolManager()
+r = http.request('GET', 'http://')
+r.status
+# 200
+r.data
+
 
