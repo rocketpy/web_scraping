a387fe9c428acb7d3c94925f57424704218ba98f
rocketpy <dimitr.ushakov@gmail.com>
2021-05-05 23:04:04 +0300

    Update User_agent_module.py

--- User_agent_module.py
+++ User_agent_module.py
@@ -1,1 +1,9 @@
+# library to generate user agent
+from user_agent import generate_user_agent
+import requests
 
+
+# generate a user agent
+headers = {'User-Agent': generate_user_agent(device_type="desktop", os=('mac', 'linux'))}
+# headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686 on x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.63 Safari/537.36'}
+page_response = requests.get(page_link, timeout=5, headers=headers)
