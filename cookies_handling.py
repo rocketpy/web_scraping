59941c93c46a54e7f95652ae73a28a1701b625a0
rocketpy <dimitr.ushakov@gmail.com>
2021-05-18 18:33:31 +0300

    Update cookies_handling.py

--- cookies_handling.py
+++ cookies_handling.py
@@ -1,1 +1,19 @@
+# biscuits - Fast and tasty cookies handling.
+
+# PyPI: https://pypi.org/project/biscuits/
+# Github: https://github.com/pyrates/biscuits
+
+# pip install biscuits
+
+#  API
+# Parse a "Cookie:" header value:
+from biscuits import parse
+parse('some=value; and=more')
+# > {'some': 'value', 'and': 'more'}
+
+# Generate a "Set-Cookie:" header value:
+from biscuits import Cookie
+cookie = Cookie(name='foo', value='bar', domain='www.example.org')
+str(cookie)
+# > "foo=bar; Domain=www.example.org; Path=/"
 
