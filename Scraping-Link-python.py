6aceae0c3fe52c2a5d5a2dd4565b0dbe7ad2f83b
rocketpy <dimitr.ushakov@gmail.com>
2021-05-24 23:32:00 +0300

    Update Scraping-Link-python.py

--- Scraping-Link-python.py
+++ Scraping-Link-python.py
@@ -1,1 +1,19 @@
+#  Scraping-Link - A Python module for Web Scraping with Scraping.link, you will be able to scrape any website and solves typical blocking complications.
+
+# PyPi:  https://pypi.org/project/scraping-link/
+# Github: https://github.com/nicolasmarin/scraping-link-python
+
+# pip install scraping-link
+
+
+# This module can be used to web scraping - Extract HTML content.
+
+from scraping_link import ScrapingLinkRequest
+
+
+url = "https://parascrapear.com/"
+apikey = "YOUR_API"
+
+r = ScrapingLinkRequest.scrape(url, apikey)
+print(r.text)
 
