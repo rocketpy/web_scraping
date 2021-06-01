424b4f3bdd9f8158bf0b69b86f29ee519d3a63db
rocketpy <dimitr.ushakov@gmail.com>
2021-06-01 18:14:02 +0300

    Update Capture_server_IP_address.py

--- Capture_server_IP_address.py
+++ Capture_server_IP_address.py
@@ -1,1 +1,16 @@
+#  Capture server IP address by Requests
 
+import requests
+
+r = requests.get("http://...")
+# r.ip
+
+"""
+rsp = requests.get('http://...', stream=True)
+# grab the IP while you can, before you consume the body !
+
+print(rsp.raw._fp.fp._sock.getpeername())
+
+# consume the body, which calls the read(), after that fileno is no longer available.
+print(rsp.content)
+"""
