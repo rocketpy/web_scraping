e53c60ad46049e86b7beebf2104404bb7cda8572
rocketpy <dimitr.ushakov@gmail.com>
2021-06-12 22:50:31 +0300

    Update Playwright_stealth.py

--- Playwright_stealth.py
+++ Playwright_stealth.py
@@ -21,4 +21,25 @@ with sync_playwright() as p:
         page.goto('http://whatsmyuseragent.org/')
         page.screenshot(path=f'example-{browser_type.name}.png')
         browser.close()
+
+
+
+# async
+
+import asyncio
+from playwright.async_api import async_playwright
+from playwright_stealth import stealth_async
+
+async def main():
+    async with async_playwright() as p:
+        for browser_type in [p.chromium, p.firefox, p.webkit]:
+            browser = await browser_type.launch()
+            page = await browser.new_page()
+            await stealth_async(page)
+            await page.goto('http://whatsmyuseragent.org/')
+            await page.screenshot(path=f'example-{browser_type.name}.png')
+            await browser.close()
+
+asyncio.get_event_loop().run_until_complete(main())
+
         
