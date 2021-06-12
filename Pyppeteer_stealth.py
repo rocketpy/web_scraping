e02268cb7f78c6a4536572b8ea52ed774f899fba
rocketpy <dimitr.ushakov@gmail.com>
2021-06-12 22:54:03 +0300

    Update Pyppeteer_stealth.py

--- Pyppeteer_stealth.py
+++ Pyppeteer_stealth.py
@@ -1,1 +1,26 @@
+# pyppeteer-stealth 
+
+# PyPi: https://pypi.org/project/pyppeteer-stealth/
+# Github: https://github.com/MeiK2333/pyppeteer_stealth
+
+# pip install pyppeteer-stealth
+
+# Test for bots : https://bot.sannysoft.com/
+
+# Usage
+
+import asyncio
+from pyppeteer import launch
+from pyppeteer_stealth import stealth
+
+async def main():
+    browser = await launch(headless=True)
+    page = await browser.newPage()
+
+    await stealth(page)  # <-- Here
+
+    await page.goto("https://bot.sannysoft.com/")
+    await browser.close()
+asyncio.get_event_loop().run_until_complete(main())
+
 
