e46be5b004cdb7ca7f08fd34efe4f0d6ba7ef647
rocketpy <dimitr.ushakov@gmail.com>
2021-06-12 22:47:48 +0300

    Update Playwright_stealth.py

--- Playwright_stealth.py
+++ Playwright_stealth.py
@@ -1,1 +1,24 @@
+# playwright stealth
 
+# PyPi: https://pypi.org/project/playwright-stealth/
+# Github: https://github.com/AtuboDad/playwright_stealth
+
+# pip install playwright-stealth
+
+
+# Usage
+
+# sync
+
+from playwright.sync_api import sync_playwright
+from playwright_stealth import stealth_sync
+
+with sync_playwright() as p:
+    for browser_type in [p.chromium, p.firefox, p.webkit]:
+        browser = browser_type.launch()
+        page = browser.new_page()
+        stealth_sync(page)
+        page.goto('http://whatsmyuseragent.org/')
+        page.screenshot(path=f'example-{browser_type.name}.png')
+        browser.close()
+        
