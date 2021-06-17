517a6e818e0534b1d4238ad2563507fe6c9032e8
rocketpy <dimitr.ushakov@gmail.com>
2021-06-17 23:23:11 +0300

    Update Scrapy_MongoDB_project.py

--- Scrapy_MongoDB_project.py
+++ Scrapy_MongoDB_project.py
@@ -1,1 +1,32 @@
+# pip install Scrapy
+# pip install pymongo
+
+# to start a new Scrapy project: scrapy startproject project_name
+# to start first spider: cd project_name
+#                        scrapy genspider example example.com
+
+# working with items: https://doc.scrapy.org/en/1.0/topics/items.html
+
+
+import scrapy
+from scrapy.item import Item, Field
+
+
+class Product(scrapy.Item):
+    name = scrapy.Field()
+    price = scrapy.Field()
+    stock = scrapy.Field()
+    last_updated = scrapy.Field(serializer=str)
+    
+
+    
+# need create a file called project_spider.py in the “spiders” directory.
+from scrapy import Spider
+
+
+class ProjectSpider(Spider):
+    name = "Some_name"
+    allowed_domains = ["blablabla.com"]
+    start_urls = ["http://...",
+                 ]
 
