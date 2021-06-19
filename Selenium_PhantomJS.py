db0aa9c2f3b8f72b9648205d8df8d46098a16119
rocketpy <dimitr.ushakov@gmail.com>
2021-06-19 23:19:53 +0300

    Update Selenium_PhantomJS.py

--- Selenium_PhantomJS.py
+++ Selenium_PhantomJS.py
@@ -1,1 +1,22 @@
+# Download PhantomJS: https://phantomjs.org/download.html
+# Installing PhantomJS on Windows: https://testguild.com/how-to-install-phantomjs/
+
+# pip install selenium
+# $ brew install phantomjs
+
+# example
+from selenium import webdriver
+
+
+driver = webdriver.PhantomJS()
+
+driver.set_window_size(1120, 550)
+driver.get("https://duckduckgo.com/")
+driver.find_element_by_id('search_form_input_homepage').send_keys("cats")
+driver.find_element_by_id("search_button_homepage").click()
+
+print(driver.current_url)
+
+driver.quit()
+
 
