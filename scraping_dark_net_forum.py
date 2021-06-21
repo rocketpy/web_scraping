01c246d84fa089dfa735b83946f54a184ba6acb3
rocketpy <dimitr.ushakov@gmail.com>
2021-06-21 22:17:28 +0300

    Create scraping_dark_net_forum.py

---
+++ scraping_dark_net_forum.py
@@ -0,0 +1,12 @@
+import queue
+import requests
+from bs4 import BeautifulSoup
+
+
+proxies = {'http': 'http://',
+           'https': 'http://'
+           }
+
+
+if __name__=='__main__':
+    print()
