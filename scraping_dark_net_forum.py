bef0895d506eb2a4af0a0f0f68858345d068c0cb
rocketpy <dimitr.ushakov@gmail.com>
2021-06-21 22:24:53 +0300

    Update scraping_dark_net_forum.py

--- scraping_dark_net_forum.py
+++ scraping_dark_net_forum.py
@@ -3,10 +3,20 @@ import requests
 from bs4 import BeautifulSoup
 
 
+URL = ''
+
 proxies = {'http': 'http://',
            'https': 'http://'
            }
 
+def get_proxie(url):
+    return requests.get(url, proxies=proxies)
+
+
+def get_data(data):
+    soup = BeautifulSoup(data, "html.parser")
+
 
 if __name__=='__main__':
-    print()
+    queue = queue.Queue()
+       
