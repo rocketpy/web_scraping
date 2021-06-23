e011ecb40df7b901ed06666c2c0b2448a7becb4b
rocketpy <dimitr.ushakov@gmail.com>
2021-06-23 22:21:31 +0300

    Update Scraping_Alax.py

--- Scraping_Alax.py
+++ Scraping_Alax.py
@@ -1,1 +1,18 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
 
+
+headrs = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0', 'accept': '*/*'}
+data = {}
+
+
+def get_data(url, params=data):
+    r = requests.get("")
+    return r
+  
+ 
+if __name__ == '__main__':
+    get_data()
+    
+    
