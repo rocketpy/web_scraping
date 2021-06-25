e03418f7de9c52331f3ab0c0096ab2dc27f435ff
rocketpy <dimitr.ushakov@gmail.com>
2021-06-25 23:31:01 +0300

    Update Using_html5lib_requests.py

--- Using_html5lib_requests.py
+++ Using_html5lib_requests.py
@@ -1,1 +1,17 @@
+#  https://pypi.org/project/html5lib/
+
+# pip install requests
+# pip install html5lib
+# pip install bs4
+
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = "http://www..."
+r = requests.get(URL)
+  
+soup = BeautifulSoup(r.content, 'html5lib')
+
+print(soup.prettify())
 
