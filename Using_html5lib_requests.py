161b441d6571cd2498d3f3d819eb21926100e8c7
rocketpy <dimitr.ushakov@gmail.com>
2021-06-25 23:46:50 +0300

    Update Using_html5lib_requests.py

--- Using_html5lib_requests.py
+++ Using_html5lib_requests.py
@@ -15,3 +15,18 @@ soup = BeautifulSoup(r.content, 'html5lib')
 
 print(soup.prettify())
 
+
+# Usage
+"""
+import html5lib
+
+with open("mydocument.html", "rb") as f:
+    document = html5lib.parse(f)
+
+# or:
+
+import html5lib
+
+document = html5lib.parse("<p>Hello World!")
+"""
+
