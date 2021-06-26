9508dbf3d16c8386b152a93f16594833fd949bde
rocketpy <dimitr.ushakov@gmail.com>
2021-06-26 23:19:28 +0300

    Update Using_html5lib_requests.py

--- Using_html5lib_requests.py
+++ Using_html5lib_requests.py
@@ -28,5 +28,21 @@ with open("mydocument.html", "rb") as f:
 import html5lib
 
 document = html5lib.parse("<p>Hello World!")
+
+# or 
+import html5lib
+
+with open("mydocument.html", "rb") as f:
+    lxml_etree_document = html5lib.parse(f, treebuilder="lxml")
+    
+
+# When using with urllib.request (Python 3), the charset from HTTP should be pass into html5lib as follows:
+
+from urllib.request import urlopen
+import html5lib
+
+with urlopen("http://example.com/") as f:
+    document = html5lib.parse(f, transport_encoding=f.info().get_content_charset())
+ 
 """
 
