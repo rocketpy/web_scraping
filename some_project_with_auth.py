19979249be5b162c0d7b874b363fc2ad17e12267
rocketpy <dimitr.ushakov@gmail.com>
2021-07-01 23:58:18 +0300

    Update some_project_with_auth.py

--- some_project_with_auth.py
+++ some_project_with_auth.py
@@ -6,6 +6,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from fake_useragent import UserAgent
+
+
+ua = UserAgent()
 
 
 # driver = webdriver.Chrome(PATH) 
