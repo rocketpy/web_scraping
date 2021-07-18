143d1421b4951447b100369767cafb983c2045c5
rocketpy <dimitr.ushakov@gmail.com>
2021-07-18 22:59:48 +0300

    Update parser_project_1.py

--- parser_project_1.py
+++ parser_project_1.py
@@ -18,5 +18,33 @@ def write_csv(data):
         writer = csv.writer(f, delimiter=',')
         writer.writerow((data['name'],
                          data['url'],
-                         data['reviews']))    
+                         data['reviews']))
+        
 
+def get_data(html):
+    parse = BeautifulSoup(html , 'lxml')
+    popular = parse.find_all('section')[1]
+    plagins = popular.find_all('article')
+
+    for i in plagins:
+        name = i.find('h2').text
+        url = i.find('h2').find('a').get('href')
+        r = i.find('span', class_='rating-count').find('a').text
+        rating = refined(r)
+
+        data = {'name': name,
+                'url': url,
+                'reviews': rating}
+
+        write_csv(data)
+
+
+def main():
+    url = 'https://www...'
+    get_data(get_html(url))
+
+
+if __name__ == '__main__':
+    main()
+
+    
