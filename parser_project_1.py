78173a44b432ce15a4e9aa651650087b31975bfc
rocketpy <dimitr.ushakov@gmail.com>
2021-07-18 22:55:25 +0300

    Update parser_project_1.py

--- parser_project_1.py
+++ parser_project_1.py
@@ -1,1 +1,15 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
 
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+
+def refined(s):
+    r = s.split(' ')[0]
+    return r.replace('.00', '')
+
+  
