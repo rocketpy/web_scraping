6d0b399ad8b31b57be574114fa1972a8d2f058fc
rocketpy <dimitr.ushakov@gmail.com>
2021-08-15 18:59:54 +0300

    Update Requests-html.py

--- Requests-html.py
+++ Requests-html.py
@@ -1,1 +1,26 @@
+# Requests-HTML: HTML Parsing for Humans
+
+
+# PyPi: https://pypi.org/project/requests-html/
+# Github: https://github.com/psf/requests-html
+
+# pip install requests-html
+
+"""
+This library intends to make parsing HTML (e.g. scraping the web) as simple and intuitive as possible.
+If you’re interested in financially supporting Kenneth Reitz open source, consider visiting this link.
+Your support helps tremendously with sustainability of motivation, as Open Source is no longer part of my day job.
+
+When using this library you automatically get:
+
+    Full JavaScript support!
+    CSS Selectors (a.k.a jQuery-style, thanks to PyQuery).
+    XPath Selectors, for the faint at heart.
+    Mocked user-agent (like a real web browser).
+    Automatic following of redirects.
+    Connection–pooling and cookie persistence.
+    The Requests experience you know and love, with magical parsing abilities.
+    Async Support
+"""
+
 
