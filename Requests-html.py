a2c3568cdd01d2919247c5423a9d9d28ea1e0292
rocketpy <dimitr.ushakov@gmail.com>
2021-08-15 19:05:10 +0300

    Update Requests-html.py

--- Requests-html.py
+++ Requests-html.py
@@ -31,3 +31,22 @@ from requests_html import HTMLSession
 session = HTMLSession()
 r = session.get('https://')
 
+
+# Try async and get some sites at the same time:
+from requests_html import AsyncHTMLSession
+
+
+asession = AsyncHTMLSession()
+
+async def get_pythonorg():
+    r = await asession.get('https://')
+
+async def get_reddit():
+    r = await asession.get('https://')
+
+async def get_google():
+    r = await asession.get('https://')
+
+result = session.run(get_pythonorg, get_reddit, get_google)
+ 
+
