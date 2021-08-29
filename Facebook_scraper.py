d9f4808a94f62cdd5f6201bf89a4a8a322064bde
rocketpy <dimitr.ushakov@gmail.com>
2021-08-29 23:25:08 +0300

    Update Facebook_scraper.py

--- Facebook_scraper.py
+++ Facebook_scraper.py
@@ -1,1 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class FBGroupScraper:
+
+    def __init__(self, group_id):
+        self.group_id = group_id
+        self.page_url = "https://mobile.facebook.com/groups/" + self.group_id
+        self.page_content = ""
+
 
