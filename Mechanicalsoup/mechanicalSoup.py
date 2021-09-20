0a7a513679e02a89323dd8a81e14ba8dd8a8a44a
rocketpy <dimitr.ushakov@gmail.com>
2021-09-20 23:07:12 +0300

    Update mechanicalSoup.py

--- Mechanicalsoup/mechanicalSoup.py
+++ Mechanicalsoup/mechanicalSoup.py
@@ -3,7 +3,36 @@
 # PyPi: https://pypi.org/project/MechanicalSoup/ 
 # Github: https://github.com/MechanicalSoup/MechanicalSoup
 # Docs: https://mechanicalsoup.readthedocs.io/en/stable/
+# Examples: https://github.com/MechanicalSoup/MechanicalSoup/tree/master/examples
 
 # pip install MechanicalSoup
 
+"""Example usage of MechanicalSoup to get the results from the Qwant
+search engine.
+"""
+
+import re
+import mechanicalsoup
+import html
+import urllib.parse
+
+
+# Connect to duckduckgo
+browser = mechanicalsoup.StatefulBrowser(user_agent='MechanicalSoup')
+browser.open("https://lite.qwant.com/")
+
+# Fill-in the search form
+browser.select_form('#search-form')
+browser["q"] = "MechanicalSoup"
+browser.submit_selected()
+
+# Display the results
+for link in browser.page.select('.result a'):
+    # Qwant shows redirection links, not the actual URL, so extract
+    # the actual URL from the redirect link:
+    href = link.attrs['href']
+    m = re.match(r"^/redirect/[^/]*/(.*)$", href)
+    if m:
+        href = urllib.parse.unquote(m.group(1))
+    print(link.text, '->', href)
 
