bde0d57a2c802d7b1f482a151903963631cd79c6
rocketpy <dimitr.ushakov@gmail.com>
2021-09-20 23:05:55 +0300

    Update mechanicalSoup.py

--- Mechanicalsoup/mechanicalSoup.py
+++ Mechanicalsoup/mechanicalSoup.py
@@ -1,1 +1,9 @@
+# MechanicalSoup - A Python library for automating interaction with websites
+
+# PyPi: https://pypi.org/project/MechanicalSoup/ 
+# Github: https://github.com/MechanicalSoup/MechanicalSoup
+# Docs: https://mechanicalsoup.readthedocs.io/en/stable/
+
+# pip install MechanicalSoup
+
 
