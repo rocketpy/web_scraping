e0008ea63daa3b58a5b8be449e62176adfc29425
rocketpy <dimitr.ushakov@gmail.com>
2021-12-01 22:38:28 +0200

    Update Scrapy_extension.py

--- Scrapy_extension.py
+++ Scrapy_extension.py
@@ -1,1 +1,15 @@
+# Spidermon 
+"""
+Spidermon is an extension for Scrapy spiders.
+The package provides useful tools for data validation, stats monitoring, and notification messages.
+This way you leave the monitoring task to Spidermon and just check the reports/notifications.
+"""
+
+# Github:https://github.com/scrapinghub/spidermon
+# Docs: https://spidermon.readthedocs.io/en/latest/installation.html
+
+# Requirements: 
+# Python 3.6, Python 3.7, Python 3.8 or Python 3.9
+
+# pip install spidermon
 
