bc1da890f8fe9c4ebddef813b7eae0a1b5f679f2
rocketpy <dimitr.ushakov@gmail.com>
2021-12-14 23:28:34 +0200

    Update requests_custom_auth.py

--- requests_custom_auth.py
+++ requests_custom_auth.py
@@ -1,1 +1,19 @@
+"""
+Authentication implementations are subclasses of AuthBase, and are easy to define.
+Requests provides two common authentication scheme implementations in requests.auth: HTTPBasicAuth and HTTPDigestAuth.
+"""
+
+from requests.auth import AuthBase
+
+
+class PizzaAuth(AuthBase):
+    """Attaches HTTP Pizza Authentication to the given Request object."""
+    def __init__(self, username):
+        # setup any auth-related data here
+        self.username = username
+
+    def __call__(self, r):
+        # modify and return the request
+        r.headers['X-Pizza'] = self.username
+        return r
 
