cf85a2301b6bea21ed5a8524742c38e75c2eaa54
rocketpy <dimitr.ushakov@gmail.com>
2021-12-23 23:28:08 +0200

    Update AutoCrawler.py

--- AutoCrawler.py
+++ AutoCrawler.py
@@ -1,1 +1,14 @@
+# https://github.com/YoongiKim/AutoCrawler
 
+# How to use
+"""
+-    Install Chrome
+
+-    pip install -r requirements.txt
+
+-    Write search keywords in keywords.txt
+
+-    Run "main.py"
+
+-    Files will be downloaded to 'download' directory.
+"""
