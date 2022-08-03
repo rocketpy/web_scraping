2749691ed86f2927c974f3dffa97aa3fad8796bc
rocketpy <dimitr.ushakov@gmail.com>
2022-08-03 23:51:58 +0300

    Update gazpacho_tutor.py

--- Gazpacho/gazpacho_tutor.py
+++ Gazpacho/gazpacho_tutor.py
@@ -1,1 +1,18 @@
+# gazpacho is a simple, fast, and modern web scraping library. 
 
+# pip install -U gazpacho
+
+from gazpacho import get, Soup
+
+
+url = 'https://scrape.world/books'
+html = get(url)
+soup = Soup(html)
+books = soup.find('div', {'class': 'book-'}, partial=True)
+
+def parse(book):
+    name = book.find('h4').text
+    price = float(book.find('p').text[1:].split(' ')[0])
+    return name, price
+
+[parse(book) for book in books]
