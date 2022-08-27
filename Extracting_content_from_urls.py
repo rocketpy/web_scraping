8991bf1a8b52cfe77740a394450f41c50ff0e6b0
rocketpy <dimitr.ushakov@gmail.com>
2022-08-27 23:34:08 +0300

    Update Extracting_content_from_urls.py

--- Extracting_content_from_urls.py
+++ Extracting_content_from_urls.py
@@ -1,1 +1,14 @@
+# micawber - a small library for extracting rich content from urls
 
+# https://pypi.org/project/micawber/
+# https://github.com/coleifer/micawber/
+
+# pip install micawber
+
+
+# what does it do?
+"""
+micawber supplies a few methods for retrieving rich metadata about a variety of links,
+such as links to youtube videos. micawber also provides functions for parsing blocks of text and
+html and replacing links to videos with rich embedded content.
+"""
