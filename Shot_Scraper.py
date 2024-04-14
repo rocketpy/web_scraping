5a051a2a2a7456617ccad3531990beace90e0b3f
rocketpy <dimitr.ushakov@gmail.com>
2024-04-14 23:50:22 +0300

    Create Shot_Scraper.py

---
+++ Shot_Scraper.py
@@ -0,0 +1,15 @@
+# shot-scraper - A command-line utility for taking automated screenshots of websites.
+
+# https://github.com/simonw/shot-scraper
+
+
+# Quick installation
+"""
+You can install the shot-scraper CLI tool using pip:
+
+pip install shot-scraper
+
+# Now install the browser it needs:
+shot-scraper install
+"""
+
