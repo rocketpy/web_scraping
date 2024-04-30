ce9c24457aa52c4a0be238c247a2ef4aea9b0abc
rocketpy <dimitr.ushakov@gmail.com>
2024-04-30 23:58:59 +0300

    Create Speaking_Search.py

---
+++ Speaking_Search.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import chromedriver_autoinstaller 
+import speech_recognition as sr
+import pyttsx3
+import pyautogui 
+
