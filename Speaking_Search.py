c63c3cf30f4704b1ba868db7f607f69a82b42828
rocketpy <dimitr.ushakov@gmail.com>
2024-05-01 23:42:58 +0300

    Update Speaking_Search.py

--- Speaking_Search.py
+++ Speaking_Search.py
@@ -1,8 +1,15 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import time
-import chromedriver_autoinstaller 
-import speech_recognition as sr
 import pyttsx3
-import pyautogui 
+import pyautogui
+from selenium import webdriver
+import speech_recognition as sr
+import chromedriver_autoinstaller
+from selenium.webdriver.common.by import By
 
+
+rec = sr.Recognizer()
+with sr.Microphone() as source_2:
+    rec.adjust_for_ambient_noise(source_2, duration = 0.2)
+    audio_2 = rec.listen(source_2)
+    my_text = rec.recognize_google(audio_2)
+    my_mext = str(my_text.lower())
