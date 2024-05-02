61b11c101c12a2d03d06022a043e912e6fd57291
rocketpy <dimitr.ushakov@gmail.com>
2024-05-02 23:50:02 +0300

    Update Speaking_Search.py

--- Speaking_Search.py
+++ Speaking_Search.py
@@ -7,9 +7,16 @@ import chromedriver_autoinstaller
 from selenium.webdriver.common.by import By
 
 
-rec = sr.Recognizer()
-with sr.Microphone() as source_2:
-    rec.adjust_for_ambient_noise(source_2, duration = 0.2)
-    audio_2 = rec.listen(source_2)
-    my_text = rec.recognize_google(audio_2)
-    my_mext = str(my_text.lower())
+while(True):
+    try:
+        rec = sr.Recognizer()
+        with sr.Microphone() as source_2:
+            rec.adjust_for_ambient_noise(source_2, duration = 0.2)
+            audio_2 = rec.listen(source_2)
+            my_text = rec.recognize_google(audio_2)
+            my_mext = str(my_text.lower())
+
+        if MyText == "search":
+            pyautogui.hotkey('ctrl', 'c')
+            chrome_options = webdriver.ChromeOptions()
+            capabilities = {'browserName': 'chrome', 'javascriptEnabled': True}
