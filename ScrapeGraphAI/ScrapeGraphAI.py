d46f3d81ba50b076dc15a6b9b81d7754c2fab0b9
rocketpy <dimitr.ushakov@gmail.com>
2024-05-09 23:31:39 +0300

    Create ScrapeGraphAI.py

---
+++ ScrapeGraphAI/ScrapeGraphAI.py
@@ -0,0 +1,3 @@
+# ScrapeGraphAI - is a web scraping python library that uses LLM and direct graph logic to create scraping pipelines for websites, documents and XML files.
+
+# https://github.com/VinciGit00/Scrapegraph-ai
