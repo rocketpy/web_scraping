605be50f2a415d2aee9dafc9e7822e5571d87eee
rocketpy <dimitr.ushakov@gmail.com>
2024-05-12 23:14:48 +0300

    Update ScrapeGraphAI.py

--- ScrapeGraphAI/ScrapeGraphAI.py
+++ ScrapeGraphAI/ScrapeGraphAI.py
@@ -1,3 +1,13 @@
 # ScrapeGraphAI - is a web scraping python library that uses LLM and direct graph logic to create scraping pipelines for websites, documents and XML files.
 
 # https://github.com/VinciGit00/Scrapegraph-ai
+
+
+# pip install scrapegraphai
+
+# you will also need to install Playwright for javascript-based scraping:
+# playwright install
+
+
+# Case 1: SmartScraper using Local Models
+# Remember to have Ollama installed and download the models using the ollama pull command.
