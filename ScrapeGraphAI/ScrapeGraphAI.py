3686c7fefea29898246c0100432fc57e5d8daec5
rocketpy <dimitr.ushakov@gmail.com>
2024-05-13 23:42:59 +0300

    Update ScrapeGraphAI.py

--- ScrapeGraphAI/ScrapeGraphAI.py
+++ ScrapeGraphAI/ScrapeGraphAI.py
@@ -11,3 +11,31 @@
 
 # Case 1: SmartScraper using Local Models
 # Remember to have Ollama installed and download the models using the ollama pull command.
+
+
+from scrapegraphai.graphs import SmartScraperGraph
+
+
+graph_config = {
+    "llm": {
+        "model": "ollama/mistral",
+        "temperature": 0,
+        "format": "json",  # Ollama needs the format to be specified explicitly
+        "base_url": "http://localhost:11434",  # set Ollama URL
+    },
+    "embeddings": {
+        "model": "ollama/nomic-embed-text",
+        "base_url": "http://localhost:11434",  # set Ollama URL
+    },
+    "verbose": True,
+}
+
+smart_scraper_graph = SmartScraperGraph(
+    prompt="List me all the projects with their descriptions",
+    # also accepts a string with the already downloaded HTML code
+    source="https://perinim.github.io/projects",
+    config=graph_config
+)
+
+result = smart_scraper_graph.run()
+# print(result)
