a72f03776f461218dc0a6585a14dfd7ae5b14c46
rocketpy <dimitr.ushakov@gmail.com>
2024-06-11 23:47:11 +0300

    Update Scrape_using_OpenAI.py

--- Scrape_using_OpenAI.py
+++ Scrape_using_OpenAI.py
@@ -17,3 +17,27 @@ asyncio.run(scrape_with_playwright(
         tags=["span"],
         schema_pydantic=SchemaNewsWebsites
     ))
+
+"""
+Setup
+1. Create a new Python virtual environment
+python -m venv virtual-env or python3 -m venv virtual-env (Mac)
+
+py -m venv virtual-env (Windows 11)
+
+2. Activate virtual environment
+.\virtual-env\Scripts\activate (Windows)
+
+source virtual-env/bin/activate (Mac)
+
+3. Install dependencies using Poetry
+Run poetry install --sync or poetry install
+
+4. Install playwright
+playwright install
+5. Create a new .env file to store OpenAI's API key
+OPENAI_API_KEY=XXXXXX
+Usage
+Run locally
+python main.py
+"""
