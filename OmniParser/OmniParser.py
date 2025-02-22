684ea465bfb3ec3ff11c089aa21696b22afbea55
rocketpy <dimitr.ushakov@gmail.com>
2025-02-22 23:56:02 +0200

    Update OmniParser.py

--- OmniParser/OmniParser.py
+++ OmniParser/OmniParser.py
@@ -2,3 +2,19 @@
 
 # https://github.com/microsoft/OmniParser
 
+
+# Install
+
+"""
+First clone the repo, and then install environment:
+
+cd OmniParser
+conda create -n "omni" python==3.12
+conda activate omni
+pip install -r requirements.txt
+Ensure you have the V2 weights downloaded in weights folder (ensure caption weights folder is called icon_caption_florence). If not download them with:
+
+# download the model checkpoints to local directory OmniParser/weights/
+for f in icon_detect/{train_args.yaml,model.pt,model.yaml} icon_caption/{config.json,generation_config.json,model.safetensors}; 
+do huggingface-cli download microsoft/OmniParser-v2.0 "$f" --local-dir weights; done mv weights/icon_caption weights/icon_caption_florence 
+"""
