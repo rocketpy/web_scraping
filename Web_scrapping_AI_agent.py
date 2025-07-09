451bce244b0371c6739d6ec030a92ada134a503d
rocketpy <dimitr.ushakov@gmail.com>
2025-07-09 22:37:00 +0300

    Create Web_scrapping_AI_agent.py

---
+++ Web_scrapping_AI_agent.py
@@ -0,0 +1,12 @@
+# Web Scrapping AI Agent
+
+
+# https://github.com/Shubhamsaboo/awesome-llm-apps/tree/main/starter_ai_agents/web_scrapping_ai_agent
+
+
+"""
+# Features:
+Scrape any website by providing the URL
+Utilize OpenAI's LLMs (GPT-3.5-turbo or GPT-4) for intelligent scraping
+Customize the scraping task by specifying what you want the AI agent to extract
+"""
