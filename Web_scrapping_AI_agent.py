1471f6a4762ff69c8f78cddf1d632ea3f5f040e6
rocketpy <dimitr.ushakov@gmail.com>
2025-07-21 23:57:23 +0300

    Update Web_scrapping_AI_agent.py

--- Web_scrapping_AI_agent.py
+++ Web_scrapping_AI_agent.py
@@ -9,4 +9,9 @@
 Scrape any website by providing the URL
 Utilize OpenAI's LLMs (GPT-3.5-turbo or GPT-4) for intelligent scraping
 Customize the scraping task by specifying what you want the AI agent to extract
+
+# Clone the GitHub repository
+
+git clone https://github.com/Shubhamsaboo/awesome-llm-apps.git
+cd awesome-llm-apps/advanced_tools_frameworks/web_scrapping_ai_agent
 """
